7c864b323d974d1350c75a9f14b6c9db8d3bb016
ThomasDDDD <thomasdtigt@googlemail.com>
2024-08-01 10:47:24 +0200

    edit: menu input

--- visualDom.js
+++ visualDom.js
@@ -1,8 +1,8 @@
 import { start } from "./index.js";
 
 let startButton = document.querySelector(`#startGame`);
-
 startButton.onclick = change;
+
 console.log(`bis hier gehts`);
 
 function change() {
